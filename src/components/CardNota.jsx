e8cf4cb7df1715be69780e875ea4bf8870ae7bc7
jonathan.Viana <jony5851@gmail.com>
2021-09-19 15:28:36 -0300

    update

--- src/components/CardNota.jsx
+++ src/components/CardNota.jsx
@@ -4,7 +4,7 @@ class CardNota extends Component {
     render() {
     return (
      <>
-       <section className="flex flex-col border-2 mt-2 ml-2 w-36 h-20 bg-green-500 justify-items-start">
+       <section className="flex flex-row border-2 mt-2 ml-2 w-36 h-20 bg-green-500 justify-items-start">
           <header>
               <h3>{this.props.title}</h3>
           </header>          
